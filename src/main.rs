e0ad8b77160fcd27c6da38f54506e9e7c9598a91
castrogarcia.mjs <castrogarcia.mjs@gmail.com>
2024-05-18 14:05:34 +0000

    Add new file in master branch

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,32 @@ fn main() {
     let name = "John";
 
     println!("Hello, {}!", name);
+
+    // add 100 print
+    for i in 1..101 {
+        println!("{}", i);
+    }
+
+    // add fundament for rust
+    println!("Hello, world!");
+    println!("Hello, world!");
+
+// Suggested code may be subject to a license. Learn more: ~LicenseLog:2728081008.
+
+    println!("Hello, world!");
+    println!("Hello, world!");
+
+
+    // Suggested code may be subject to a license. Learn more: ~LicenseLog:2728081008.
+
+    println!("Hello, world!");
+    println!("Hello, world!");
+
+    // Suggested code may be subject to a license. Learn more: ~LicenseLog:2728081008.
+
+    println!("Hello, world!");
+    println!("Hello, world!");
+    // greet function
     greet()
 }
 
