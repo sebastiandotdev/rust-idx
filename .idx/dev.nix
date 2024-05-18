cfd8fe5f302e6f1d9cf3d7dc9ea6e9aacbb625be
castrogarcia.mjs <castrogarcia.mjs@gmail.com>
2024-05-18 14:02:00 +0000

    Add example IDE `idx`

---
+++ .idx/dev.nix
@@ -0,0 +1,28 @@
+# To learn more about how to use Nix to configure your environment
+# see: https://developers.google.com/idx/guides/customize-idx-env
+{ pkgs, ... }: {
+  # Which nixpkgs channel to use.
+  channel = "stable-23.11"; # or "unstable"
+  # Use https://search.nixos.org/packages to find packages
+  packages = [
+    pkgs.cargo
+    pkgs.rustc
+    pkgs.rustfmt
+    pkgs.stdenv.cc
+  ];
+  # Sets environment variables in the workspace
+  env = {
+    RUST_SRC_PATH = "${pkgs.rustPlatform.rustLibSrc}";
+  };
+  idx = {
+    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+    extensions = [
+      "rust-lang.rust-analyzer"
+      "tamasfe.even-better-toml"
+      "serayuzgur.crates"
+      "vadimcn.vscode-lldb"
+    ];
+    # Enable previews and customize configuration
+    previews = {};
+  };
+}
---
+++ src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    println!("Hello, world!");
+    println!("Hello, world!");
+    
+    let name = "John";
+
+    println!("Hello, {}!", name);
+    greet()
+}
+
+fn greet() {
+    println!("Hello, world!")
+}
